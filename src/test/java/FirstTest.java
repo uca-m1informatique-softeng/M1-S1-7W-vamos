496f4e7653e362edb3467f086fb125251533409c
zn508116 <zn508116@etu.unice.fr>
2020-09-23 18:43:57 +0200

    First test

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FirstTest {
+
+    @Test
+    void mavenPass() {
+        assertEquals("Test", "Test");
+    }
+}
