361b4ac28549c340c067405511dec2338db86980
SMoumen <moumensofian@gmail.com>
2020-09-24 15:39:20 +0200

    #15 Javadoc for constante class

--- src/main/java/Utility/Constante.java
+++ src/main/java/Utility/Constante.java
@@ -1,11 +1,14 @@
 package Utility;
 
+/**
+ * This class will contains all const values (String,Integers..) needed to make the code clearer
+ */
 public final class Constante {
     private Constante(){}
 
 
+    // Core game const values
     public static int MAX_AGE = 3;
-    public static int MIN_CARDS =2;
     public static int MAX_ROUNDS =6;
     public static int MAX_HAND = 7;
 }
