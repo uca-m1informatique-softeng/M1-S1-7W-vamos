f53f0ca7d854925baaec4cf054456ec85fba5274
Sonja Deißenböck <sonja@deissenboeck.org>
2020-09-24 16:46:45 +0200

    trying out json parsing.. can't access path so far

--- src/main/java/Core/Card.java
+++ src/main/java/Core/Card.java
@@ -1,7 +1,8 @@
 package Core;
 
+import org.json.*;
 import java.util.EnumMap;
-
+import java.nio.file.*;
 public class Card {
 
     private String name;
@@ -12,4 +13,11 @@ public class Card {
     private EnumMap<Resource, Integer> resource;
     private EnumMap<Resource, Integer> cost;
 
+    public static void main(String[] args){
+        String content = Files.readString(Paths.get("src/assets/cards/blue/altar.json"));
+     JSONObject json = new JSONObject(content);
+        System.out.println(json);
+//String name = json.getJSONObject(0).getString("name");
+  //   System.out.println(name);
+    }
 }
