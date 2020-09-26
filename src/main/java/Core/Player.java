327f22ab97ffdd199629d05e946287c8ddb12a14
Ossama <ossama1998@windowslive.com>
2020-09-26 22:35:23 +0200

    Getting resources from cards #26

--- src/main/java/Core/Player.java
+++ src/main/java/Core/Player.java
@@ -118,65 +118,70 @@ public class Player {
             }
         }
 
-        if (enoughResources){
+        Boolean freeCard=false ;
+        if(this.chosenCard.getCost().get(Resource.WOOD) ==0 & this.chosenCard.getCost().get(Resource.STONE) ==0 & this.chosenCard.getCost().get(Resource.ORE) ==0
+            &this.chosenCard.getCost().get(Resource.CLAY) ==0 & this.chosenCard.getCost().get(Resource.GLASS) ==0 & this.chosenCard.getCost().get(Resource.LOOM) ==0
+            & this.chosenCard.getCost().get(Resource.PAPYRUS) ==0){
+                freeCard=true ;
+            }
+        if(freeCard){ //free card
             this.builtCards.add(this.chosenCard);
-
-            //points given by a card
-            int currentVP = this.points.get(CardPoints.VICTORY);
-            int cardVP = this.chosenCard.getCardPoints().get(CardPoints.VICTORY);
-            this.points.put(CardPoints.VICTORY, currentVP + cardVP);
-
-            int currentMP = this.points.get(CardPoints.MILITARY);
-            int cardMP = this.chosenCard.getCardPoints().get(CardPoints.MILITARY);
-            this.points.put(CardPoints.MILITARY, currentMP + cardMP);
-
-            int currentSWP = this.points.get(CardPoints.SCIENCE_WHEEL);
-            int cardSWP = this.chosenCard.getCardPoints().get(CardPoints.SCIENCE_WHEEL);
-            this.points.put(CardPoints.SCIENCE_WHEEL, currentSWP + cardSWP);
-
-            int currentSTP = this.points.get(CardPoints.SCIENCE_TABLET);
-            int cardSTP = this.chosenCard.getCardPoints().get(CardPoints.SCIENCE_TABLET);
-            this.points.put(CardPoints.SCIENCE_TABLET, currentSTP + cardSTP);
-
-            int currentSCP = this.points.get(CardPoints.SCIENCE_COMPASS);
-            int cardSCP = this.chosenCard.getCardPoints().get(CardPoints.SCIENCE_COMPASS);
-            this.points.put(CardPoints.SCIENCE_COMPASS, currentSCP + cardSCP);
-
-            //adding resource(s) if the card gives a ressource(s)
-            int currentWOOD=this.resources.get(Resource.WOOD);
-            int cardWOOD=this.chosenCard.getResource().get(Resource.WOOD);
-            this.resources.put(Resource.WOOD, currentWOOD + cardWOOD);
-
-            int currentSTONE=this.resources.get(Resource.STONE);
-            int cardSTONE=this.chosenCard.getResource().get(Resource.STONE);
-            this.resources.put(Resource.STONE, currentSTONE + cardSTONE);
-
-            int currentORE=this.resources.get(Resource.ORE);
-            int cardORE=this.chosenCard.getResource().get(Resource.ORE);
-            this.resources.put(Resource.ORE, currentORE + cardORE);
-
-            int currentCLAY=this.resources.get(Resource.CLAY);
-            int cardCLAY=this.chosenCard.getResource().get(Resource.CLAY);
-            this.resources.put(Resource.CLAY, currentCLAY + cardCLAY);
-
-            int currentGLASS=this.resources.get(Resource.GLASS);
-            int cardGLASS=this.chosenCard.getResource().get(Resource.GLASS);
-            this.resources.put(Resource.GLASS, currentGLASS + cardGLASS);
-
-            int currentLOOM=this.resources.get(Resource.LOOM);
-            int cardLOOM=this.chosenCard.getResource().get(Resource.LOOM);
-            this.resources.put(Resource.LOOM, currentLOOM + cardLOOM);
-
-            int currentPAPYRUS=this.resources.get(Resource.PAPYRUS);
-            int cardPAPYRUS=this.chosenCard.getResource().get(Resource.PAPYRUS);
-            this.resources.put(Resource.PAPYRUS, currentPAPYRUS + cardPAPYRUS);
-
+            addPointsAndResources();
             this.hand.remove(this.chosenCard);
+        }
+        else{ //not a free card sowe check if the player have enough resources to build the card
+            if (enoughResources){
+                this.builtCards.add(this.chosenCard);
+                addPointsAndResources();
 
-            System.out.println(this.name + " played the card " + this.chosenCard.getName() + " and got " + cardVP + " victory points.");
+                //removing the cost of a card if it's not a free card
+                for(Resource resource : this.chosenCard.getCost().keySet()){
+                    this.resources.put(resource, this.resources.get(resource) - this.chosenCard.getCost().get(resource)  );
+                }
+
+                this.hand.remove(this.chosenCard);
+            }
+            else{ //if the player don't have enough resources to buy the card he toss it
+                System.out.println("Not enough ressources");
+                this.dumpCard();
+            }
         }
 
-
-
     }
+
+    public void addPointsAndResources(){
+        //adding points if the card gives a points
+        int currentVP = this.points.get(CardPoints.VICTORY);
+        int cardVP = this.chosenCard.getCardPoints().get(CardPoints.VICTORY);
+        this.points.put(CardPoints.VICTORY, currentVP + cardVP);
+
+        int currentMP = this.points.get(CardPoints.MILITARY);
+        int cardMP = this.chosenCard.getCardPoints().get(CardPoints.MILITARY);
+        this.points.put(CardPoints.MILITARY, currentMP + cardMP);
+
+        int currentSWP = this.points.get(CardPoints.SCIENCE_WHEEL);
+        int cardSWP = this.chosenCard.getCardPoints().get(CardPoints.SCIENCE_WHEEL);
+        this.points.put(CardPoints.SCIENCE_WHEEL, currentSWP + cardSWP);
+
+        int currentSTP = this.points.get(CardPoints.SCIENCE_TABLET);
+        int cardSTP = this.chosenCard.getCardPoints().get(CardPoints.SCIENCE_TABLET);
+        this.points.put(CardPoints.SCIENCE_TABLET, currentSTP + cardSTP);
+
+        int currentSCP = this.points.get(CardPoints.SCIENCE_COMPASS);
+        int cardSCP = this.chosenCard.getCardPoints().get(CardPoints.SCIENCE_COMPASS);
+        this.points.put(CardPoints.SCIENCE_COMPASS, currentSCP + cardSCP);
+
+        //adding resource(s) if the card gives a ressource(s)
+        for(Resource resource : this.chosenCard.getResource().keySet()){
+            int currentResource=this.resources.get(resource);
+            int cardResource=this.chosenCard.getResource().get(resource);
+            this.resources.put(resource, currentResource + cardResource);
+            if (cardResource!=0){
+                System.out.println(this.name + " played the card " + this.chosenCard.getName() + " and got " + cardResource +" " + resource );
+            }
+        }
+
+        System.out.println(this.name + " played the card " + this.chosenCard.getName() + " and got " + cardVP + " victory points.");
+    }
+
 }
