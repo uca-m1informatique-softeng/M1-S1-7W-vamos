88b427c62d4799b6d2bc0aa5e18f0384feb65a8b
SMoumen <moumensofian@gmail.com>
2020-09-28 11:05:19 +0200

    #38 - Added Exceptions package and created ParserException class

---
+++ src/main/java/Exceptions/ParserException.java
@@ -0,0 +1,6 @@
+package Exceptions;
+
+public class ParserException extends RuntimeException
+{
+    public ParserException(String msg) { super(msg);}
+}
--- src/main/java/Utility/Utilities.java
+++ src/main/java/Utility/Utilities.java
@@ -1,18 +1,18 @@
 package Utility;
 
+import Exceptions.ParserException;
+
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.text.ParseException;
 import java.util.Scanner;
 
 public class Utilities {
 
-    public static void displayGameOutput() throws ParseException {
+    public static void displayGameOutput(){
         Scanner sc;
         try
         {
             sc = new Scanner(new File(Writer.NOM_FICHIER));
-            if(!sc.hasNextLine()) System.out.println("empty scanner");
             while (sc.hasNextLine())
             {
                 System.out.println(sc.nextLine());
@@ -21,7 +21,7 @@ public class Utilities {
             sc.close();
         } catch (FileNotFoundException e)
         {
-            throw new ParseException("Erreur lors de l'affichage du déroulement du jeu.",0);
+            throw new ParserException("Game state couldn't correctly be displayed - File not found");
         }
     }
 }
