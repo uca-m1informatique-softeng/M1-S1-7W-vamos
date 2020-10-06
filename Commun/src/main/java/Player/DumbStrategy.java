2c0e5231c2b90cfe29477cda033220756ffcb009
fpoutre <kevin@fpout.re>
2020-10-06 13:48:35 +0200

    Implemented DumbStrategy

---
+++ Commun/src/main/java/Player/DumbStrategy.java
@@ -0,0 +1,29 @@
+package Player;
+
+import java.security.SecureRandom;
+
+/**
+ * This class extends Strategy.
+ * DumbStrategy is a random strategy implementation, that should represent a "dumb" player.
+ */
+public class DumbStrategy extends Strategy {
+
+    /**
+     * Random values generator
+     */
+    SecureRandom rand = new SecureRandom();
+
+    /**
+     * Implementation of Strategy::chooseAction(Player player).
+     * This implementation chooses a random card and a random action available from its hand.
+     * @param player The player whose action should be chosen.
+     * @return A random action.
+     */
+    @Override
+    public Action chooseAction(Player player) {
+        int index = rand.nextInt(player.hand.size());
+        int action = rand.nextInt(3) + 1;
+
+        return new Action(player.getHand().get(index), action);
+    }
+}
