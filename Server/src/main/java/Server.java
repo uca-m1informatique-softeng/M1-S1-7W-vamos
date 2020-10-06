7f03bd60a0013165bc2b9e08eeedea9b48ee85f2
Ossama <ossama1998@windowslive.com>
2020-10-06 21:35:51 +0200

    Server javadoc #15

--- Server/src/main/java/Server.java
+++ Server/src/main/java/Server.java
@@ -1,28 +1,30 @@
-import com.corundumstudio.socketio.AckRequest;
 import com.corundumstudio.socketio.Configuration;
 import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.listener.ConnectListener;
-import com.corundumstudio.socketio.listener.DataListener;
-
-import java.util.logging.Logger;
 
 import static Utility.Constante.* ;
 
+/**
+ *  Server class : it runs the server and
+ *  receive data from the client
+ */
 public class Server {
     public static void main(String[] args) {
         Configuration config = new Configuration();
         config.setHostname("127.0.0.1");
         config.setPort(10101);
 
-        // creation du serveur
+        /**
+         *   Variable socketIOServer, the server.
+         */
         SocketIOServer server = new SocketIOServer(config);
         server.start();
 
         server.addConnectListener(new ConnectListener() {
             @Override
             public void onConnect(SocketIOClient socketIOClient) {
-                System.out.println("Server and Socket are connected");
+                System.out.println("Server and Socket are connected...");
             }
         });
 
