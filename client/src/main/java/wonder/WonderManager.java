9bb4b30b138978a1a3434193aefedd86176ea564
fpoutre <kevin@fpout.re>
2020-10-25 17:22:18 +0100

    fixed 2 major code smells in wonder.WonderManager

--- client/src/main/java/wonder/WonderManager.java
+++ client/src/main/java/wonder/WonderManager.java
@@ -13,6 +13,8 @@ import static java.nio.charset.StandardCharsets.UTF_8;
  */
 public class WonderManager {
 
+    private WonderManager() {}
+
     public static ArrayList<Wonder> parseWonders() throws IOException {
 
         ArrayList<Wonder> wondersList = new ArrayList<>();
@@ -34,50 +36,6 @@ public class WonderManager {
             Writer.write("Couldn't read wonders directory !");
         }
 
-        /*
-        File folder = new File("resources/wonders");
-
-        File[] listOfFiles = folder.listFiles(new FileFilter() {
-            @Override
-            public boolean accept(File pathname) {
-                String name = pathname.getName().toLowerCase();
-                return name.endsWith(".json") && pathname.isFile();
-            }
-        });
-        ArrayList<String> wonderNames = new ArrayList<String>();
-        for (File file : listOfFiles) {
-            //check whether the wonder either in form A or B already exists in fileNames
-            String wonderName = (file.getName().replace(".json", "")).substring(0, (file.getName().replace(".json", "")).length() - 1);
-            if (!wonderNames.contains(wonderName)) {
-                wonderNames.add(wonderName);
-            }
-        }
-        if (wonderNames.size() < Game.getPlayers()) {
-            throw new WondersException("There aren't enough wonders for each player please lower the number of players and restart the game. exiting..");
-        }
-        String fileName = "";
-
-        for (int i = 0; i < listOfFiles.length; i++) {
-            if (listOfFiles[i].isFile() && !listOfFiles[i].getName().contains(".DS_Store")) {
-                fileName = listOfFiles[i].getName();
-            } else if (listOfFiles[i].isDirectory()) {
-                fileName = listOfFiles[i].getName();
-            }
-            if (!fileName.equals("") &&
-                    //FOR TESTING
-                    (!fileName.contains("alexandria")&&!fileName.contains("gizah")
-                            &&!fileName.contains("ephesos") && !fileName.contains("halikarnassos"))) {
-                try {
-                    wonder wonder = new wonder(fileName.replace(".json", ""));
-                    wondersList.add(wonder);
-                    if (Game.debug) {
-                        Writer.write(wonder.getName() + "has been created");
-                    }
-                } catch (Exception e) {
-                    Writer.write(fileName + " could not be read.");
-                }
-            }
-        }*/
         return wondersList;
     }
 }
