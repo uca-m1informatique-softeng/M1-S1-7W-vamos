5d722bc08c87ebdb571462d3ff512927ea264400
Ossama <ossama1998@windowslive.com>
2020-12-28 14:13:29 +0100

    Class AmbitiousStrategy

---
+++ client/src/main/java/player/AmbitiousStrategy.java
@@ -0,0 +1,14 @@
+package player;
+
+public class AmbitiousStrategy extends Strategy{
+
+    @Override
+    public Action chooseAction(Player player) {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+}
