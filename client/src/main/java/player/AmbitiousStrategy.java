92605f4f894a18515e3d2f868d080382c7d6653a
zn508116 <zn508116@etu.unice.fr>
2020-12-28 15:05:50 +0100

    first commit

--- client/src/main/java/player/AmbitiousStrategy.java
+++ client/src/main/java/player/AmbitiousStrategy.java
@@ -1,5 +1,7 @@
 package player;
 
+import core.Game;
+
 public class AmbitiousStrategy extends Strategy{
 
     @Override
@@ -10,5 +12,9 @@ public class AmbitiousStrategy extends Strategy{
     @Override
     public String toString() {
         return "Ambitious AI";
+    }
+
+    private int simul(Action a, Game game){
+        return -1;
     }
 }
